1b8b762629749c7d53ebe1a461163dd842a19005
Aleksey Antonov <tasuke34@gmail.com>
2018-07-15 20:50:07 +0300

    15.07

---
+++ project1/app/src/main/java/com/android/killqwerty/project1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.android.killqwerty.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.idtxtHello);
+    }
+}
