93530dc22bb849559ec21d22c666bbbba75eff54
Aleksey Antonov <tasuke34@gmail.com>
2018-07-19 23:18:19 +0300

    19.07

--- project1/app/src/main/java/com/android/killqwerty/project1/MainActivity.java
+++ project1/app/src/main/java/com/android/killqwerty/project1/MainActivity.java
@@ -2,16 +2,23 @@ package com.android.killqwerty.project1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    TextView textView, textView2;
+  //  ImageView imageView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.idtxtHello);
+ //       textView =  findViewById(R.id.idtxtHello);
+//        textView2 = findViewById(R.id.Gfy);
+//    //    imageView = findViewById(R.id.imageView);
+//        button = findViewById(R.id.button);
     }
 }
