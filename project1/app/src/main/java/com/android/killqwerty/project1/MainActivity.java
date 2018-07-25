6ab1eca0f6c9dc4d3a29b9ef1e5f37a5a540a075
Aleksey Antonov <tasuke34@gmail.com>
2018-07-25 21:26:48 +0300

    25.07

--- project1/app/src/main/java/com/android/killqwerty/project1/MainActivity.java
+++ project1/app/src/main/java/com/android/killqwerty/project1/MainActivity.java
@@ -4,21 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView, textView2;
-  //  ImageView imageView;
+//    TextView ivText;
     Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- //       textView =  findViewById(R.id.idtxtHello);
-//        textView2 = findViewById(R.id.Gfy);
-//    //    imageView = findViewById(R.id.imageView);
+//        String[] array = this.getResources().getStringArray(R.array.my_str_array);
+//        StringBuilder sBuilder = new StringBuilder();
+//        for (int i = 0; i < array.length; i++)
+//            sBuilder.append(array[i] + "\n");
+//        ivText.setText(sBuilder.toString());
         button = findViewById(R.id.button);
     }
 }
