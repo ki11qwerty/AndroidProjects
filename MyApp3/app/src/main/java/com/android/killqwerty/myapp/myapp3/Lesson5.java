8df543cfa1f21860afca6006341c391afa4a41c5
Aleksey Antonov <tasuke34@gmail.com>
2018-09-13 21:48:28 +0300

    13.09

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Lesson5.java
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Lesson5.java
@@ -1,7 +1,6 @@
 package com.android.killqwerty.myapp.myapp3;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -21,12 +20,15 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Lesson5 extends AppCompatActivity {
+    static final short LENGTH_IDS_ARRAY = 23;
     private ArrayList<Person> persons;
     private LinearLayout allPersons;
+    public int[] idForDrawable;
     ListView listView;
     View ListLayout;
     View ListLayoutWithListView;
     Button buttPrev, buttList, buttListAdapter, buttGrid, buttTusk;
+    static long time;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,8 +55,11 @@ public class Lesson5 extends AppCompatActivity {
             public void onClick(View view) {
                 setContentView(ListLayout);
                 allPersons = findViewById(R.id.all_persons);
+                time = System.currentTimeMillis();
                 createPersonList(100);
                 fillList();
+                Toast.makeText(getApplicationContext(),""+(System.currentTimeMillis()-time)
+                        +"ms",Toast.LENGTH_LONG).show();
 
             }
         });
@@ -64,8 +69,11 @@ public class Lesson5 extends AppCompatActivity {
             public void onClick(View view) {
                 setContentView(ListLayoutWithListView);
                 listView = findViewById(R.id.lv_persons_listview);
+                time = System.currentTimeMillis();
                 createPersonList(5000);
                 fillListWithAdaptor();
+                Toast.makeText(getApplicationContext(),""+(System.currentTimeMillis()-time)
+                        +"ms",Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -78,7 +86,9 @@ public class Lesson5 extends AppCompatActivity {
             p.setName("Qwerty" + i);
             p.setEmail("Qwerty" + i + "@gmail.com");
             p.setTelephone("8-555-" + (1000000 + random.nextInt(8999999)));
+
             p.setAvatar(createRandomAvatar());
+
             persons.add(p);
         }
     }
@@ -124,58 +134,80 @@ public class Lesson5 extends AppCompatActivity {
     }
 
     private Drawable createRandomAvatar() {
+        if(idForDrawable == null)
+            fillIdForDrawable();
         Random random = new Random();
-        int num = 1 + random.nextInt(23);
-        switch (num) {            // надо как то укоротить эту столбень потом =) этож пздц-----------
-            case 1:
-                return getResources().getDrawable(R.drawable.friend1);
-            case 2:
-                return getResources().getDrawable(R.drawable.friend2);
-            case 3:
-                return getResources().getDrawable(R.drawable.friend3);
-            case 4:
-                return getResources().getDrawable(R.drawable.friend4);
-            case 5:
-                return getResources().getDrawable(R.drawable.friend5);
-            case 6:
-                return getResources().getDrawable(R.drawable.friend6);
-            case 7:
-                return getResources().getDrawable(R.drawable.friend7);
-            case 8:
-                return getResources().getDrawable(R.drawable.friend8);
-            case 9:
-                return getResources().getDrawable(R.drawable.friend9);
-            case 10:
-                return getResources().getDrawable(R.drawable.friend10);
-            case 11:
-                return getResources().getDrawable(R.drawable.friend11);
-            case 12:
-                return getResources().getDrawable(R.drawable.friend12);
-            case 13:
-                return getResources().getDrawable(R.drawable.friend13);
-            case 14:
-                return getResources().getDrawable(R.drawable.friend14);
-            case 15:
-                return getResources().getDrawable(R.drawable.friend15);
-            case 16:
-                return getResources().getDrawable(R.drawable.friend16);
-            case 17:
-                return getResources().getDrawable(R.drawable.friend17);
-            case 18:
-                return getResources().getDrawable(R.drawable.friend18);
-            case 19:
-                return getResources().getDrawable(R.drawable.friend19);
-            case 20:
-                return getResources().getDrawable(R.drawable.friend20);
-            case 21:
-                return getResources().getDrawable(R.drawable.friend21);
-            case 22:
-                return getResources().getDrawable(R.drawable.friend22);
-            case 23:
-                return getResources().getDrawable(R.drawable.friend23);
+        int num = random.nextInt(LENGTH_IDS_ARRAY);
+        return getResources().getDrawable(idForDrawable[num]);
 
+//--------------------так выходило затратно по времени >1000ms для каждого вызова-------------------
+
+//        Random random = new Random();
+//        int num = 1 + random.nextInt(23);
+//        String drawableName = "friend"+num;
+//        int res = getResources().getIdentifier(drawableName,"drawable",getPackageName());
+//        return getResources().getDrawable(res);
+
+// ------------------а так бесила длинна кода для заполнения но хорошая скорость 1000ms для первого
+// ------------------запуска и 350ms для последующих-----------------------------------------------
+//        switch (num) {
+//            case 1:
+//                return getResources().getDrawable(R.drawable.friend1);
+//            case 2:
+//                return getResources().getDrawable(R.drawable.friend2);
+//            case 3:
+//                return getResources().getDrawable(R.drawable.friend3);
+//            case 4:
+//                return getResources().getDrawable(R.drawable.friend4);
+//            case 5:
+//                return getResources().getDrawable(R.drawable.friend5);
+//            case 6:
+//                return getResources().getDrawable(R.drawable.friend6);
+//            case 7:
+//                return getResources().getDrawable(R.drawable.friend7);
+//            case 8:
+//                return getResources().getDrawable(R.drawable.friend8);
+//            case 9:
+//                return getResources().getDrawable(R.drawable.friend9);
+//            case 10:
+//                return getResources().getDrawable(R.drawable.friend10);
+//            case 11:
+//                return getResources().getDrawable(R.drawable.friend11);
+//            case 12:
+//                return getResources().getDrawable(R.drawable.friend12);
+//            case 13:
+//                return getResources().getDrawable(R.drawable.friend13);
+//            case 14:
+//                return getResources().getDrawable(R.drawable.friend14);
+//            case 15:
+//                return getResources().getDrawable(R.drawable.friend15);
+//            case 16:
+//                return getResources().getDrawable(R.drawable.friend16);
+//            case 17:
+//                return getResources().getDrawable(R.drawable.friend17);
+//            case 18:
+//                return getResources().getDrawable(R.drawable.friend18);
+//            case 19:
+//                return getResources().getDrawable(R.drawable.friend19);
+//            case 20:
+//                return getResources().getDrawable(R.drawable.friend20);
+//            case 21:
+//                return getResources().getDrawable(R.drawable.friend21);
+//            case 22:
+//                return getResources().getDrawable(R.drawable.friend22);
+//            case 23:
+//                return getResources().getDrawable(R.drawable.friend23);
+//
+//        }
+//        return getResources().getDrawable(R.drawable.friend1);
+    }
+    public void fillIdForDrawable(){
+        idForDrawable = new int[LENGTH_IDS_ARRAY];
+        String nameRes = "friend";
+        for (int i = 0;i<idForDrawable.length; i++){
+            idForDrawable[i] = getResources().getIdentifier(nameRes + i,
+                    "drawable",getPackageName());
         }
-        return getResources().getDrawable(R.drawable.friend1);
     }
 
     class Person {
@@ -220,7 +252,7 @@ public class Lesson5 extends AppCompatActivity {
     public class PersonAdapter extends BaseAdapter {
         ArrayList<Person> persons;
         Context c;
-        public PersonAdapter(ArrayList<Person> persons, Context c) {
+        private PersonAdapter(ArrayList<Person> persons, Context c) {
             this.persons = persons;
             this.c = c;
         }
