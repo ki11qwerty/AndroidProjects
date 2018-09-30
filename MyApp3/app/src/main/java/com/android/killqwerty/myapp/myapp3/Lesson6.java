3e46ba5f635eb9dd3dd466aa210094a12eb47199
Aleksey Antonov <tasuke34@gmail.com>
2018-10-01 00:28:08 +0300

    01.10

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Lesson6.java
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Lesson6.java
@@ -1,14 +1,17 @@
 package com.android.killqwerty.myapp.myapp3;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Lesson6 extends AppCompatActivity {
-    Button buttPrev;
+    Button buttPrev, buttIntentNewActivity;
+    EditText textToIntent;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         setContentView(R.layout.lesson6);
@@ -21,6 +24,21 @@ public class Lesson6 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+        textToIntent = findViewById(R.id.lesson6_edit_text_1);
+        buttIntentNewActivity = findViewById(R.id.lesson6_button_new_activity);
+        buttIntentNewActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textToIntent.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(),"поле не может быть пустым",
+                            Toast.LENGTH_SHORT).show();
+                String thisText = textToIntent.getText().toString();
+                Intent intent = new Intent(Lesson6.this,Lesson6SecondActivity.class);
+                intent.putExtra("KEY1",thisText);
+                setResult(RESULT_OK, intent);
+                startActivity(intent);
             }
         });
     }
--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Lesson6SecondActivity.java
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Lesson6SecondActivity.java
@@ -6,32 +6,32 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Lesson6SecondActivity extends AppCompatActivity {
     Button buttPrev, buttNext;
+    TextView txtFromIntent;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lesson6_second_activity);
         setMyButtons();
+        setTextInActivity();
     }
     public void setMyButtons() {
         buttPrev = findViewById(R.id.button_backward_2);
         buttPrev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Lesson6SecondActivity.this, AboutMe.class);
-                startActivity(intent);
+                finish();
             }
         });
+    }
+    public void setTextInActivity(){
+        String someText = getIntent().getExtras().getString("KEY1");
+        txtFromIntent = findViewById(R.id.lesson6_text_from_intent);
+        txtFromIntent.setTextSize(18);
+        txtFromIntent.setText(someText);
 
-//        buttNext = findViewById(R.id.button_forward_2);
-//        buttNext.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(Lesson6SecondActivity.this, Activity3th.class);
-//                startActivity(intent);
-//            }
- //       });
     }
 }
