76ddee0e24a6166abcf31bb0f37c49cd0eee951c
Aleksey Antonov <tasuke34@gmail.com>
2018-11-29 22:08:50 +0300

    29.11

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Android2/Lesson1.java
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Android2/Lesson1.java
@@ -13,9 +13,20 @@ import android.widget.Toast;
 
 import com.android.killqwerty.myapp.myapp3.R;
 
+import java.util.Random;
+
 public class Lesson1 extends AppCompatActivity {
     public static final String DB_NAME = "MyDataBaseLesson1";
     public static final String MY_TAG = "MyLogs";
+    String[] names = {"Иванов","Петров","Сидоров","Антонов","Песков","Никульшин","Ивлеев",
+            "Захаров","Марченко","Путин","Медведев","Навальный","Дробинин","Винярский","Ильченко",
+            "Крюков"};
+    String[] i_o = {"А.А","А.В","Е.Г","О.Ю","Г.О","П.П","П.В","Н.О","Е.А","О.Г","В.Е","А.Н","А.Б","Б.Ю"};
+    String[] posts = {"Напальник отдела","уборщик","бармен","тестировщик","царь во дворца",
+            "помощник директора","системный администратор","официант","халдей","повар","шеф-повар",
+            "администратор","бухгалтер","закупщик","грузчик","программист","доставщик",
+            "поставщик","кладовщик","старший бухгалтер","SEO","фотограф","водитель","стажер",
+            "директор",};
     View.OnClickListener listener;
     Button btnAdd, btnLoad, btnDelete, btnExample;
     EditText etFio, etAge, etPost, etCost;
@@ -33,9 +44,13 @@ public class Lesson1 extends AppCompatActivity {
         btnAdd = findViewById(R.id.btn_a2_l1_add);
         btnDelete = findViewById(R.id.btn_a2_l1_delete);
         btnLoad = findViewById(R.id.btn_a2_l1_load);
+        btnExample = findViewById(R.id.btn_a2l1_example);
+
         btnAdd.setOnClickListener(listener);
         btnDelete.setOnClickListener(listener);
         btnLoad.setOnClickListener(listener);
+        btnExample.setOnClickListener(listener);
+
         etFio = findViewById(R.id.a2l1_et_f_i_o);
         etAge = findViewById(R.id.a2l1_et_age);
         etPost = findViewById(R.id.a2l1_et_post);
@@ -63,31 +78,32 @@ public class Lesson1 extends AppCompatActivity {
                         Log.d(MY_TAG, "row inserted, ID = " + rowID);
                         break;
                     case R.id.btn_a2_l1_load:
-                        loadDb();
                         break;
                     case R.id.btn_a2_l1_delete:
                         int clearCount = db.delete("mytable", null, null);
                         Log.d(MY_TAG, "deleted rows count = " + clearCount);
                         break;
+                    case R.id.btn_a2l1_example:
+                        createRandomFields();
                 }
                 dbHelper.close();
+                // TODO: допилить кнопку екзампл - в свичь, рандом генерацию персоны, и усе
             }
         };
 
     }
 
-    public void addToDb() {
-      //  SQLiteDatabase db = dbHelper.getWritableDatabase();
+    public void createRandomFields() {
+        Random rand = new Random();
+        String age = ""+(rand.nextInt(43) + 18);
+        String cost = ""+(((rand.nextInt(24)+1) * 1000) + 20000);
+        String post = posts[rand.nextInt(posts.length)];
+        String fio = names[rand.nextInt(names.length)] +" "+ i_o[rand.nextInt(i_o.length)];
+        etAge.setText(age);
+        etFio.setText(fio);
+        etCost.setText(cost);
+        etPost.setText(post);
 
-        Toast.makeText(getApplicationContext(),"add",Toast.LENGTH_SHORT).show();
-    }
-
-    public void loadDb() {
-        Toast.makeText(getApplicationContext(),"laod",Toast.LENGTH_SHORT).show();
-    }
-
-    public void deleteDb() {
-        Toast.makeText(getApplicationContext(),"delete",Toast.LENGTH_SHORT).show();
     }
 
 }
