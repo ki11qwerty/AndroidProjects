e4e4910da415243e549202dc739dc09fd5ecbe17
Aleksey Antonov <tasuke34@gmail.com>
2019-01-23 20:51:34 +0300

    23.01.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Android2/Lesson2.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/Android2/Lesson2.kt
@@ -2,8 +2,10 @@ package com.android.killqwerty.myapp.myapp3.Android2
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.android.killqwerty.myapp.myapp3.R
 import kotlinx.android.synthetic.main.android2_lesson2.*
 
@@ -18,12 +20,13 @@ class Lesson2: Activity() {
     }
 
     private fun setMyButtonsAndViews() {
-        val exit: Button = findViewById(R.id.andr2_lesson2_btn_exit)
+        val exit = findViewById<Button>(R.id.andr2_lesson2_btn_exit)
         exit.setOnClickListener { finish() }
-        val tv1: TextView = findViewById(R.id.andr2_lesson2_tv1)
+        val tv1 = findViewById<TextView>(R.id.andr2_lesson2_tv1)
         tv1.text = text
         tv1.textSize = 24f
-        tv1.setTextColor(resources.getColor(R.color.colorGreen))
+        tv1.setTextColor(ContextCompat.getColor(this,R.color.colorGreen))
+        Toast.makeText(this,"this is my first activity with Kotlin", Toast.LENGTH_LONG).show()
     }
     private fun someFun(){
 
