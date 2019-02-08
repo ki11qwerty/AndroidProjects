6e57ff34a0371eab0aed5b849a64a68d902b59fa
Aleksey Antonov <tasuke34@gmail.com>
2019-02-09 01:13:26 +0300

    09.02.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
@@ -1,36 +1,35 @@
 package com.android.killqwerty.myapp.myapp3.android_2
 
 import android.os.Bundle
+import android.support.annotation.NonNull
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AppCompatActivity
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.widget.Toast
 import com.android.killqwerty.myapp.myapp3.R
-import com.android.killqwerty.myapp.myapp3.R.*
-import com.google.android.gms.maps.MapFragment
-import com.google.android.gms.maps.SupportMapFragment
 
 import kotlinx.android.synthetic.main.android2_lesson2.*
+import org.jetbrains.annotations.NotNull
 
 class Lesson2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.android2_lesson2)
+        setContentView(R.layout.android2_lesson2)
         setMyButtons()
         setMap()
     }
-    //не пойму че к чему, завтра додумаю не пошло...
 
     fun setMyButtons() {
         Toast.makeText(this,"${(andr2_les2_edText.text)}", Toast.LENGTH_LONG).show()
 // ключ AIzaSyDAINF9rLW_w2TgslOI-bf-dHuTiOjGRi4
     }
     fun setMap(){
-//        val fragment1 = Lesson2_map_fragment()
-//        val mapManager : MapFragment? = MapFragment()
-//        mapManager.
-//                .add(R.id.andr2_les2_fragment,fragment1) // непонятно чет нифига... завтра допилю
-//                .commit()
+        val fragment1 = Lesson2_map_fragment()
+        val fragmentManager1 : FragmentManager = supportFragmentManager
+        fragmentManager1.beginTransaction()
+                .add(R.id.andr2_les2_fragment_in_map_layout,fragment1)
+                .commit()
     }
 }
--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2_map_fragment.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2_map_fragment.kt
@@ -2,6 +2,7 @@ package com.android.killqwerty.myapp.myapp3.android_2
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
