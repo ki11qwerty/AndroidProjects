2c71a43d689ff61b920b3cf6465dc0fb7873120a
Aleksey Antonov <tasuke34@gmail.com>
2019-02-09 13:52:47 +0300

    09.02.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
@@ -1,19 +1,24 @@
 package com.android.killqwerty.myapp.myapp3.android_2
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.annotation.NonNull
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentActivity
-import android.support.v7.app.AppCompatActivity
 import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
 import com.android.killqwerty.myapp.myapp3.R
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 import kotlinx.android.synthetic.main.android2_lesson2.*
-import org.jetbrains.annotations.NotNull
 
-class Lesson2 : AppCompatActivity() {
+class Lesson2 : Activity() {
+//    override fun onMapReady(googleMap: GoogleMap?) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        googleMap?.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker"))
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.android2_lesson2)
@@ -23,13 +28,10 @@ class Lesson2 : AppCompatActivity() {
 
     fun setMyButtons() {
         Toast.makeText(this,"${(andr2_les2_edText.text)}", Toast.LENGTH_LONG).show()
-// ключ AIzaSyDAINF9rLW_w2TgslOI-bf-dHuTiOjGRi4
     }
     fun setMap(){
-        val fragment1 = Lesson2_map_fragment()
-        val fragmentManager1 : FragmentManager = supportFragmentManager
-        fragmentManager1.beginTransaction()
-                .add(R.id.andr2_les2_fragment_in_map_layout,fragment1)
-                .commit()
-    }
+//       // val myMap = findViewById(R.id.andr2_les2_fragment_in_map_layout) as MapView
+//        val myMap2 = fragmentManager.findFragmentById(R.id.andr2_les2_fragment_in_map_layout) as MapView
+//        myMap2.getMapAsync(this)
+     }
 }
