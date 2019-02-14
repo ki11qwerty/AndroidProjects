1f84f933899f52d7ec063a5c781df917462b694f
Aleksey Antonov <tasuke34@gmail.com>
2019-02-14 15:01:18 +0300

    14.02.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
@@ -1,26 +1,17 @@
 package com.android.killqwerty.myapp.myapp3.android_2
 
-import android.app.Activity
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
-import android.support.v4.app.FragmentManager
-import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.view.WindowManager
 import com.android.killqwerty.myapp.myapp3.R
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_main_android2.*
-
-import kotlinx.android.synthetic.main.android2_lesson2.*
 
 class Lesson2 : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.
+
         setContentView(R.layout.android2_lesson2)
         val fragment = Lesson2_Fragment()
         val fragManager = supportFragmentManager
