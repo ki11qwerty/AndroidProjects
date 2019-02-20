e88d25d1306e902b0138847a72db577b56d8fe0d
Aleksey Antonov <tasuke34@gmail.com>
2019-02-20 13:47:07 +0300

    20.02.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2.kt
@@ -10,8 +10,6 @@ class Lesson2 : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-        window.
-
         setContentView(R.layout.android2_lesson2)
         val fragment = Lesson2_Fragment()
         val fragManager = supportFragmentManager
--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2_Fragment.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson2_Fragment.kt
@@ -1,19 +1,19 @@
 package com.android.killqwerty.myapp.myapp3.android_2
 
-import android.app.Application
-import android.content.pm.PackageManager
-import android.location.Location
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import com.android.killqwerty.myapp.myapp3.R
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.CameraPosition
 import kotlinx.android.synthetic.main.android2_lesson2_some_fragment.*
+
 
 class Lesson2_Fragment : Fragment() , OnMapReadyCallback, View.OnClickListener{
     lateinit var myView : View
@@ -40,24 +40,49 @@ class Lesson2_Fragment : Fragment() , OnMapReadyCallback, View.OnClickListener{
         googleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
         val settings = googleMap.uiSettings
         settings.isZoomControlsEnabled = true
-        //TODO: короче закоменчю пока что это дело. завтра допилить все это дело с получением кординат, маркеры, ну и пробки можно добавить
-        //todo  : Допилить кнопку, при нажатии сделать видимым меню и скрыть кнопку, а там уже маркеры
-//        settings.isMapToolbarEnabled = true
-//        settings.isMyLocationButtonEnabled = true
-//        if(ContextCompat.checkSelfPermission(myView.context,"android.permission.ACCESS_COARSE_LOCATION") == PackageManager.PERMISSION_GRANTED)
-//            googleMap.isMyLocationEnabled = true
-      //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        googleMap.addMarker(MarkerOptions()
+                .position(volgogradMap)
+                .title("Волгоград"))
+
+        googleMap.addMarker(MarkerOptions()
+                .position(steakHouseMap)
+                .title("Steak House"))
+
+        googleMap.addMarker(MarkerOptions()
+                .position(homeMap)
+                .title("Home"))
+
+      changeCamera(volgogradMap, 10f)
+
     }
     fun setButtons(){
-     //   andr2_l2_btn_my_location.setOnClickListener{ onClick(it) }
+        andr2_les2_btn_work.setOnClickListener{ onClick(it) }
+        andr2_les2_btn_home.setOnClickListener { onClick(it) }
+        andr2_les2_volgograd.setOnClickListener { onClick(it) }
     }
-override fun onClick(view: View?) {
-        when(view?.id){
-         //   andr2_l2_btn_my_location.id -> Toast.makeText(myView.context,"чпонь", Toast.LENGTH_LONG).show()
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            andr2_les2_btn_home.id -> changeCamera(homeMap, 17f)
+            andr2_les2_btn_work.id -> changeCamera(steakHouseMap, 17f)
+            andr2_les2_volgograd.id -> changeCamera(volgogradMap, 10f)
         }
+    }
+    fun changeCamera(cordinats: LatLng, zoom: Float){
+        val cameraPosition = CameraPosition.Builder()
+                .target(cordinats)
+                .zoom(zoom)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+        myGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
     companion object {
         lateinit var myMap: MapView
         lateinit var myGoogleMap: GoogleMap
+        val volgogradMap = LatLng(48.7085007,44.5149151)
+        val steakHouseMap = LatLng(48.7047823,44.5170374)
+        val homeMap = LatLng(48.7587845,44.5057762)
     }
 }
