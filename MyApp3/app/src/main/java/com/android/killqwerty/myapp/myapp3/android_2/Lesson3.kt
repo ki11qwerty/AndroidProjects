9a21ee08e35764e07cc7055a2d4475cbeb85d982
Aleksey Antonov <tasuke34@gmail.com>
2019-03-18 17:42:58 +0300

    18.03.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson3.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson3.kt
@@ -1,30 +1,108 @@
 package com.android.killqwerty.myapp.myapp3.android_2
 
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import com.android.killqwerty.myapp.myapp3.R
 import kotlinx.android.synthetic.main.android2_lesson3.*
-import kotlinx.coroutines.runBlocking
 
 class Lesson3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.android2_lesson3)
-        setMyButtons()
+        setMyViews()
     }
 
-    fun setMyButtons() {
+    fun setMyViews() {
         andr2_lesson3_btn_exit.setOnClickListener { finish() }
         andr2_les3_btn_start.setOnClickListener { onClick(it) }
         andr2_les3_btn_stop.setOnClickListener { onClick(it) }
+
+        andr2_les3_btn_launch1.setOnClickListener { onClick(it) }
+        andr2_les3_btn_launch2.setOnClickListener { onClick(it) }
+        andr2_les3_btn_launch3.setOnClickListener { onClick(it) }
     }
 
     fun onClick(view: View) {
         when (view.id) {
-            andr2_les3_btn_start.id -> startService(Intent(this, Lesson3Service::class.java))
+            andr2_les3_btn_start.id -> {
+                startService(Intent(this, Lesson3Service::class.java))
+                Toast.makeText(this,"Всю движуху смотреть в логах, \n тут ничего не видно =)", Toast.LENGTH_LONG).show()}
             andr2_les3_btn_stop.id -> stopService(Intent(this, Lesson3Service::class.java))
+            andr2_les3_btn_launch1.id -> {
+                val pi = createPendingResult(TASK_1,Intent(),0)
+                val intent = Intent(this,Lesson3Service::class.java)
+                        .putExtra(PARAM_TIME,7)
+                        .putExtra(PARAM_PINTENT, pi)
+                startService(intent)
+            }
+            andr2_les3_btn_launch2.id -> {
+                val pi = createPendingResult(TASK_2,Intent(),0)
+                val intent = Intent(this,Lesson3Service::class.java)
+                        .putExtra(PARAM_TIME,4)
+                        .putExtra(PARAM_PINTENT, pi)
+                startService(intent)
+            }
+            andr2_les3_btn_launch3.id -> {
+                val pi = createPendingResult(TASK_3,Intent(),0)
+                val intent = Intent(this,Lesson3Service::class.java)
+                        .putExtra(PARAM_TIME,2)
+                        .putExtra(PARAM_PINTENT, pi)
+                startService(intent)
+            }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == STATUS_START){
+            when(requestCode){
+                TASK_1 -> {
+                    andr2_les3_tv_status1.text = "Start"
+                    andr2_les3_tv_status1.setTextColor(resources.getColor(R.color.colorGreen))}
+                TASK_2 -> {
+                    andr2_les3_tv_status2.text = "Start"
+                    andr2_les3_tv_status2.setTextColor(resources.getColor(R.color.colorGreen)) }
+                TASK_3 -> {
+                    andr2_les3_tv_status3.text = "Start"
+                    andr2_les3_tv_status3.setTextColor(resources.getColor(R.color.colorGreen))}
+            }
+        }
+        if(resultCode == STATUS_FINISH){
+            var result = data?.getIntExtra(PARAM_RESULT,0)
+            when(requestCode){
+                TASK_1 -> {andr2_les3_tv_status1.text = "Finish $result" }
+                TASK_2 -> {andr2_les3_tv_status2.text = "Finish $result"}
+                TASK_3 -> {andr2_les3_tv_status3.text = "Finish $result"}
+            }
+        }
+        if(resultCode == STATUS_CLEAR ){
+            when(requestCode){
+                TASK_1 -> {
+                    andr2_les3_tv_status1.text = "ожидание"
+                    andr2_les3_tv_status1.setTextColor(resources.getColor(R.color.colorBlack))}
+                TASK_2 -> {
+                    andr2_les3_tv_status2.text = "ожидание"
+                    andr2_les3_tv_status2.setTextColor(resources.getColor(R.color.colorBlack)) }
+                TASK_3 -> {
+                    andr2_les3_tv_status3.text = "ожидание"
+                    andr2_les3_tv_status3.setTextColor(resources.getColor(R.color.colorBlack))}
+            }
+        }
+    }
+
+    companion object {
+        val TASK_1 = 1
+        val TASK_2 = 2
+        val TASK_3 = 3
+        val STATUS_START = 100
+        val STATUS_FINISH = 200
+        val STATUS_CLEAR = 300
+        val PARAM_TIME = "time"
+        val PARAM_PINTENT = "pendingIntent"
+        val PARAM_RESULT = "result"
     }
 }
--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson3Service.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson3Service.kt
@@ -1,28 +1,34 @@
 package com.android.killqwerty.myapp.myapp3.android_2
 
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
+import java.util.function.IntToLongFunction
 
 class Lesson3Service : Service() {
     override fun onCreate() {
         Log.d(MYTAG, "onCreate started")
-        myJob = GlobalScope.launch { myFun() }
+       // myJob = GlobalScope.launch { myFun() }
         super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (!myJob.isActive)
-            myJob.start()
-        Log.d(MYTAG, "onStartCommand started")
+        var time = intent?.getIntExtra(PARAM_TIME,0)
+        var pi = intent?.getParcelableExtra<PendingIntent>(PARAM_PINTENT)
+        GlobalScope.launch { newJob(time,startId,pi) }.start()
+//
+//        if (!myJob.isActive)
+//            myJob.start()
+//        Log.d(MYTAG, "onStartCommand started")
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         Log.d(MYTAG, "onDestroy service")
-        myJob.cancel()
+      //  myJob.cancel()
         super.onDestroy()
     }
 
@@ -42,9 +48,31 @@ class Lesson3Service : Service() {
         }
     }
 
+    suspend fun newJob(time: Int? ,startId : Int,pi : PendingIntent?){
+        Log.d("MYTAG","вошел в метод newJob time = $time pi = $pi startid = $startId ")
+        pi?.send(STATUS_START)
+        if (time != null) {
+            delay((time * 1000).toLong())
+        var myIntent = Intent().putExtra(PARAM_RESULT, time * 1000)
+            pi?.send(this,STATUS_FINISH,myIntent)
+            delay(3000)
+            pi?.send(STATUS_CLEAR)
+            stopSelf()}
+    }
+
     companion object {
         val MYTAG = "MYTAG"
         lateinit var myJob : Job
+        val TASK_1 = 1
+        val TASK_2 = 2
+        val TASK_3 = 3
+        val STATUS_START = 100
+        val STATUS_FINISH = 200
+        val STATUS_CLEAR = 300
+        val PARAM_TIME = "time"
+        val PARAM_PINTENT = "pendingIntent"
+        val PARAM_RESULT = "result"
+
     }
 }
 
