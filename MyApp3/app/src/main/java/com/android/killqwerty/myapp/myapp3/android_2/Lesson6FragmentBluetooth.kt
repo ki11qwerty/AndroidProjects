2f92d40025ee90949ea341a2c7264d5208db6604
Aleksey Antonov <tasuke34@gmail.com>
2019-03-30 23:07:46 +0300

    30.03.19

---
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson6FragmentBluetooth.kt
@@ -0,0 +1,7 @@
+package com.android.killqwerty.myapp.myapp3.android_2
+
+import android.support.v4.app.Fragment
+
+class Lesson6FragmentBluetooth : Fragment(){
+
+}
---
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson6FragmentServie.kt
@@ -0,0 +1,8 @@
+package com.android.killqwerty.myapp.myapp3.android_2
+
+import android.support.v4.app.Fragment
+
+class Lesson6FragmentServie : Fragment(){
+
+}
+
