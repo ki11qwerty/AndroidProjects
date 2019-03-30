1d545daeaca9ca9ad03225b5c08dc97370ca789d
Aleksey Antonov <tasuke34@gmail.com>
2019-03-30 23:12:34 +0300

    30.03.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson6FragmentBluetooth.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson6FragmentBluetooth.kt
@@ -1,7 +1,10 @@
 package com.android.killqwerty.myapp.myapp3.android_2
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
 
 class Lesson6FragmentBluetooth : Fragment(){
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
