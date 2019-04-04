6cbad7e3cf821d4a81c56c5129265d1bedc0d53a
Aleksey Antonov <tasuke34@gmail.com>
2019-04-05 01:43:32 +0300

    05.04.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson6PageFragment.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson6PageFragment.kt
@@ -2,6 +2,7 @@ package com.android.killqwerty.myapp.myapp3.android_2
 
 import android.app.Application
 import android.content.ContentResolver
+import android.content.Context
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.os.Bundle
@@ -13,12 +14,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.android.killqwerty.myapp.myapp3.R
 import java.util.jar.Manifest
+import java.util.zip.Inflater
 
 class Lesson6PageFragment : Fragment() {
     var pageNumber: Int? = 0
@@ -51,10 +50,10 @@ class Lesson6PageFragment : Fragment() {
     }
 
     fun configureFirstFragment(view: View) { // тут будет настройка смс фрагмента
-        //var listOfItem : MutableList<Item> = MutableList(100,)
+        val listOfItem = mutableListOf<Item>()
         view.findViewById<Button>(R.id.button1).setOnClickListener { Toast.makeText(context, "чпонь кнопка 1", Toast.LENGTH_SHORT).show() }
         //TODO: захуярить сюда проверку разрешения, пока включил в ручную эт пиздец
-        //val listView = view.findViewById<ListView>(R.id.a2l6_listview)
+        val listView = view.findViewById<ListView>(R.id.a2l6_listview)
         //val cr = object : ContentResolver(context){}
         if(ContextCompat.checkSelfPermission(context!!.applicationContext, android.Manifest.permission.READ_SMS)
             != PackageManager.PERMISSION_GRANTED){
@@ -73,11 +72,13 @@ class Lesson6PageFragment : Fragment() {
                     textIDX = c.getColumnIndex(Telephony.Sms.BODY)
                     address = c.getString(addressIDX)
                     text = c.getString(textIDX)
-                    Log.d("MYTAG", "$address :::: $text\n\n")
-                  //  listOfItem.add(Item(address,text))
+                   // Log.d("MYTAG", "$address :::: $text\n\n")
+                    listOfItem.add(Item(address,text))
                 } while (c.moveToNext())
                 c.close()
             }
+            listView.adapter = SmsAdapter(listOfItem,this.activity!!.applicationContext) // todo тут чет с контексотом херня нужно нот нул
+
         }
     }
 
@@ -102,3 +103,27 @@ class Lesson6PageFragment : Fragment() {
     }
 }
 data class Item(var address :String,var body : String)
+class SmsAdapter(val items : MutableList<Item>, var c : Context) : BaseAdapter(){
+
+    override fun getCount(): Int {
+       return items.size
+    }
+
+    override fun getItem(position: Int): Any {
+       return items[position]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+    override fun getView(position: Int,convertView: View?, parent: ViewGroup?): View {
+        var myView = convertView
+        if(myView == null) {
+            myView = LayoutInflater.from(c).inflate(R.layout.android2_lesson6_item, null)
+        }
+
+        myView!!.findViewById<TextView>(R.id.a2l6_item_address).text = items[position].address
+        myView!!.findViewById<TextView>(R.id.a2l6_item_body).text = items[position].body
+        return myView
+    }
+}
