ea9983f15664759f59b090cb3a432ce3bf632fe8
Aleksey Antonov <tasuke34@gmail.com>
2019-06-03 13:22:17 +0300

    03.06.19

--- MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson7.kt
+++ MyApp3/app/src/main/java/com/android/killqwerty/myapp/myapp3/android_2/Lesson7.kt
@@ -7,15 +7,17 @@ import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.TranslateAnimation
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.Toast
 import com.android.killqwerty.myapp.myapp3.R
 
-// TODO: осталась анимация на дом
 
 class Lesson7 : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.android2_lesson7)
         var display : Display = windowManager.defaultDisplay
@@ -35,6 +37,36 @@ class Lesson7 : Activity() {
         myImageView.setBackgroundResource(R.drawable.anim)
         val animation = myImageView.background as AnimationDrawable
         animation.start()
+
+        val sky1 = findViewById<ImageView>(R.id.a2l7_sky)
+        val sky2 = findViewById<ImageView>(R.id.a2l7_sky2)
+        val ground = findViewById<ImageView>(R.id.a2l7_ground)
+
+        val animationSky1 = TranslateAnimation(5000f,-5000f,0f,0f)
+        animationSky1.duration = 25000
+        animationSky1.fillAfter = true
+        animationSky1.repeatMode = Animation.RESTART
+        animationSky1.repeatCount = Animation.INFINITE
+        animationSky1.interpolator = LinearInterpolator()
+
+        val animationSky2 = TranslateAnimation(4000f,-4000f,0f,0f)
+        animationSky2.duration = 15000
+        animationSky2.fillAfter = true
+        animationSky2.repeatMode = Animation.RESTART
+        animationSky2.repeatCount = Animation.INFINITE
+        animationSky2.interpolator = LinearInterpolator()
+
+        val myAnimationForGround = TranslateAnimation(2000f,-2000f,0f,0f)
+        myAnimationForGround.duration = 50000
+        myAnimationForGround.fillAfter = true
+        myAnimationForGround.repeatMode = Animation.RESTART
+        myAnimationForGround.repeatCount = Animation.INFINITE
+        myAnimationForGround.interpolator = LinearInterpolator()
+
+
+        sky1.startAnimation(animationSky1)
+        sky2.startAnimation(animationSky2)
+        ground.startAnimation(myAnimationForGround)
     }
     companion object{
         var xDisplay: Int = -1
@@ -84,7 +116,7 @@ class MyDraw(c: Context) : View(c){
 }
 //Домашнее задание
 // Разобрать все примеры из урока
-// Реализовать компонент UI который
+// Реализовать компонент UI который               НУ ВРОДЕ БЫ ВСЕ ОК! XD
 //может рисовать на экране одну из
 //простых геометрических фигур
 //(окружность, квадрат, треугольник),
