3169fac695141729942ccf8bd5b8f1d8c0985ca8
Aleksey Antonov <tasuke34@gmail.com>
2019-06-07 11:47:17 +0300

    07.06.19

---
+++ RetrofitTraining/app/src/main/java/com/android/killqwerty/myapp/retrofittraining/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.killqwerty.myapp.retrofittraining
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+
+    }
+}
