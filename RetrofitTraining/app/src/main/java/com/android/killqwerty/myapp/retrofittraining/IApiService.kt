6760cec0cfa671b95f523d6b95ea01b635ec42f7
Aleksey Antonov <tasuke34@gmail.com>
2019-06-28 01:32:28 +0300

    28.06.19

---
+++ RetrofitTraining/app/src/main/java/com/android/killqwerty/myapp/retrofittraining/IApiService.kt
@@ -0,0 +1,28 @@
+package com.android.killqwerty.myapp.retrofittraining
+
+import com.google.gson.GsonBuilder
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+import java.util.*
+
+interface IApiService {
+    // тут будет гет и тому подобное но потом
+    @GET("posts")
+    fun getAllPosts() : Observable<List<Post>>
+
+    companion object Factory {
+        fun create(): IApiService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create()) // GsonBuilder().create()
+                .build()
+            return retrofit.create(IApiService::class.java)
+        }
+    }
+}
--- RetrofitTraining/app/src/main/java/com/android/killqwerty/myapp/retrofittraining/MainActivity.kt
+++ RetrofitTraining/app/src/main/java/com/android/killqwerty/myapp/retrofittraining/MainActivity.kt
@@ -2,23 +2,31 @@ package com.android.killqwerty.myapp.retrofittraining
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.gson.GsonBuilder
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.internal.schedulers.IoScheduler
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        findViewById<TextView>(R.id.main_tv).text = "RetrofitTraining\n начнется завтра =)"
-        findViewById<Button>(R.id.main_btn1).setOnClickListener {
-            Toast.makeText(this,"Чпонь",Toast.LENGTH_LONG).show()
-        }
 
+        val postsApi = IApiService.create()
+        val response =  postsApi.getAllPosts()
+        response.subscribe {
+            Log.d("MYTAG", "${it.size}")
+        }
     }
 
     override fun onDestroy() {
-        Toast.makeText(this,"booom!",Toast.LENGTH_LONG).show()
         super.onDestroy()
     }
 }
