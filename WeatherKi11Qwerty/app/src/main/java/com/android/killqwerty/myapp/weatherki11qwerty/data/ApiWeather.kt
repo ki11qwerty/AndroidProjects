737140a050f471c06c44fe05ddb760a2a7a7f2f5
Aleksey Antonov <tasuke34@gmail.com>
2019-07-15 15:46:22 +0300

    15.07.19

--- WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/data/ApiWeather.kt
+++ WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/data/ApiWeather.kt
@@ -1,6 +1,12 @@
 package com.android.killqwerty.myapp.weatherki11qwerty.data
 
+import com.android.killqwerty.myapp.weatherki11qwerty.data.response.CurrentWeatherResponse
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.CallAdapter
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,8 +17,33 @@ interface ApiWeather{
     fun getCurrentWeather(
         @Query("q") location: String,
         @Query("lang") language: String = "en"
-    )
+    ) : Deferred<CurrentWeatherResponse>
 
     companion object{
-    }//TODO: ты знаешь что нужно сделать
+    operator fun invoke() : ApiWeather{
+        val ceptor = Interceptor{chain ->
+            val url = chain.request()
+                .url()
+                .newBuilder()
+                .addQueryParameter("key", api_key)
+                .build()
+            val request = chain.request()
+                .newBuilder()
+                .url(url)
+                .build()
+            return@Interceptor chain.proceed(request)
+        }
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(ceptor)
+            .build()
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://api.apixu.com/v1/")
+            .addCallAdapterFactory() //TODO короче сил нет думать сегодня, https://medium.com/nuances-of-programming/android-networking-%D0%B2-2019-retrofit-%D1%81-kotlin-coroutines-7254e68ca4a4
+            // вот там поискать завтра ответы, как с депрекейтед КоротинсАдаптерФактори быть...
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiWeather::class.java)
+
+    }}
 }
