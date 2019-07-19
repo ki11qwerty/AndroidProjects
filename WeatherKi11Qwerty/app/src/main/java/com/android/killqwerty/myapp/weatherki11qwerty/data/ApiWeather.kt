82efa8c06af36bfbe516bda419c8e6239c4dd508
Aleksey Antonov <tasuke34@gmail.com>
2019-07-19 20:44:49 +0300

    19.07.19

--- WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/data/ApiWeather.kt
+++ WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/data/ApiWeather.kt
@@ -4,7 +4,6 @@ import com.android.killqwerty.myapp.weatherki11qwerty.data.response.CurrentWeath
 import kotlinx.coroutines.Deferred
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import retrofit2.CallAdapter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -14,7 +13,7 @@ const val api_key = "f3b6066e6df24b029e891754191407"
 //https://api.apixu.com/v1/current.json?key=f3b6066e6df24b029e891754191407&q=Volgograd
 interface ApiWeather{
     @GET("current.json")
-    fun getCurrentWeather(
+    suspend fun getCurrentWeather(
         @Query("q") location: String,
         @Query("lang") language: String = "en"
     ) : Deferred<CurrentWeatherResponse>
@@ -39,8 +38,6 @@ interface ApiWeather{
         return Retrofit.Builder()
             .client(okHttpClient)
             .baseUrl("https://api.apixu.com/v1/")
-            //.addCallAdapterFactory() //TODO короче сил нет думать сегодня, https://medium.com/nuances-of-programming/android-networking-%D0%B2-2019-retrofit-%D1%81-kotlin-coroutines-7254e68ca4a4
-            // вот там поискать завтра ответы, как с депрекейтед КоротинсАдаптерФактори быть...
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiWeather::class.java)
--- WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/ui/MainActivity.kt
+++ WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/ui/MainActivity.kt
@@ -2,14 +2,31 @@ package com.android.killqwerty.myapp.weatherki11qwerty.ui
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
+import android.widget.TextView
 import com.android.killqwerty.myapp.weatherki11qwerty.R
+import com.android.killqwerty.myapp.weatherki11qwerty.data.ApiWeather
+import com.android.killqwerty.myapp.weatherki11qwerty.data.response.CurrentWeatherResponse
+import kotlinx.coroutines.*
+import retrofit2.Retrofit
 
 class MainActivity : Activity() {
+    lateinit var response : CurrentWeatherResponse
+    private val weatherApi = ApiWeather()
     override fun onCreate(savedInstanceState: Bundle?) {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.main_activity)
+        init()
+    }
+    fun init(){
+        GlobalScope.launch(Dispatchers.Main){
+            response = weatherApi.getCurrentWeather("Volgograd").await()
+            //TODO = Registering an InstanceCreator with Gson for this type may fix this problem. похоже я тут надолго
+            //findViewById<TextView>(R.id.my_tv_1).text = response.currentWeatherEntry.toString()
+            delay(100L)
+            Log.d("myTag","after delay!")
+        }
     }
 }
