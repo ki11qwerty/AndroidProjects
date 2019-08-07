2df949d82eacb29b3c69ab88ebdf57512cfcef6f
Aleksey Antonov <tasuke34@gmail.com>
2019-08-07 11:02:12 +0300

    07.08.19

--- WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/ui/MainActivity.kt
+++ WeatherKi11Qwerty/app/src/main/java/com/android/killqwerty/myapp/weatherki11qwerty/ui/MainActivity.kt
@@ -1,8 +1,8 @@
 //-------------------------------------------------------------------------
 //             По существу:
-// готово! - сделать обработку ошибок в запросе
-// ну и это чутка сделал, запрос-ответ есть) осталось подшаманить дизайн наверное) todo: реализовать прогноз погоды на ближайшие дни
-// todo: 3 - реализовать список погоды
+// готово 1 - сделать обработку ошибок в запросе
+// готово 2 - реализовать прогноз погоды на ближайшие дни
+// готово 3 - реализовать список погоды
 // todo: 4 - подшаманить архитектуру, LiveData или что то попроще, добавить слой
 // todo: 5-  добавить локальное сохранение предыдущих показаний, проверять последнее обновление   .. + показывать старую информацию  если нет сети
 // todo: 6 - после добавления дата слоя, реализовать сервис который будет обновлять информацию хотя бы рад в день
@@ -32,34 +32,39 @@ import kotlinx.coroutines.*
 import retrofit2.HttpException
 
 class MainActivity : Activity() {
-    lateinit var weatherResponse : CurrentWeatherResponse
+    lateinit var weatherResponse: CurrentWeatherResponse
     lateinit var forecastResponse: ForecastResponse
-    lateinit var myImage : ImageView
-    val defaultCity : String = "Волгоград"
-    var city : String = defaultCity
+    lateinit var myImage: ImageView
+    val defaultCity: String = "Волгоград"
+    var city: String = defaultCity
     lateinit var forecastListView: ListView
     lateinit var adapterForList: AdapterForList
 
     private val weatherApi = ApiWeather()
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         forecastListView = findViewById(R.id.my_forecast_list)
         myImage = findViewById(R.id.condition_icon)
         init()
         update_btn.setOnClickListener {
-            Log.d("myTag","${city_et.text}")
-            if(city_et.text.isNotEmpty())
+            Log.d("myTag", "${city_et.text}")
+            if (city_et.text.isNotEmpty())
                 city = city_et.text.toString()
-            init() }
+            init()
+        }
     }
+
     @SuppressLint("SetTextI18n") // иду  на это осознанно так как наврятли я это приложение буду переводить на другие языки, и суть учебы на данный момент совсем в другом
     fun init() {
         GlobalScope.launch(Dispatchers.Main) {
             try {
                 weatherResponse = weatherApi.getCurrentWeather(city, "ru")
-                forecastResponse = weatherApi.getForecastWeather(city,language = "ru")
+                forecastResponse = weatherApi.getForecastWeather(city, language = "ru")
             } catch (e: HttpException) {                                // ловим и тостим ошибку, ставим город по умолчанию, делаем запрос повторно
                 when (e.code()) {                                       // оставлю WHEN если понадобится обработать разные ошибки после
                     in 400..451 -> {
@@ -67,7 +72,11 @@ class MainActivity : Activity() {
                         city = defaultCity
                         init()
                     }
-                    in 500..507 -> Toast.makeText(applicationContext, "ошибка сервера : ${e.code()}", Toast.LENGTH_LONG).show()
+                    in 500..507 -> Toast.makeText(
+                        applicationContext,
+                        "ошибка сервера : ${e.code()}",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
             Log.d("myTag", "${weatherResponse.currentWeatherEntry}")
@@ -84,24 +93,16 @@ class MainActivity : Activity() {
                 .fit()
                 .into(myImage)
 
-                Log.d("MYTAG", "${forecastResponse.forecast.forecastday.size}")
+            Log.d("MYTAG", "${forecastResponse.forecast.forecastday.size}")
 
-                var myList: MutableList<Forecastday> = mutableListOf()
-                for (list in forecastResponse.forecast.forecastday) {
-                    myList.add(list)
-                }
-                adapterForList = AdapterForList(myList, applicationContext)
-                forecastListView.adapter = adapterForList
+            var myList: MutableList<Forecastday> = mutableListOf()
+            for (list in forecastResponse.forecast.forecastday) {
+                myList.add(list)
+            }
+            adapterForList = AdapterForList(myList, applicationContext)
+            forecastListView.adapter = adapterForList
 
 
-//            for(days in myList){
-//                for(singleDay in days){
-//                    val myView: View = LayoutInflater.from(applicationContext).inflate(R.layout.forecast_item,null)
-//                    myView.item_date_tv.text = singleDay.day.mintempC.toString()
-//
-//
-//                    Log.d("myTag","${singleDay.day}") //todo: доделать , вот тут допилить или метод или прям тут запилить заполнение списка форкаста
-//                }
         }
     }
 }
